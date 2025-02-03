be53b994421513acb34d2205ca94b4f0f46f1034
kamrul <cdkamrul9@gmail.com>
2025-02-03 06:15:29 +0600

    1409a

---
+++ cp-jan/1409a.py
@@ -0,0 +1,16 @@
+import sys
+
+# sys.stdout = open('cp-jan/output.txt', 'w')
+# sys.stdin = open('cp-jan/input.txt', 'r')
+
+input = sys.stdin.readline
+
+gi = lambda: list(map(int, input().split()))
+gs = lambda: list(input().split())
+
+t = int(input())
+
+for i in range(t):
+    a, b = gi()
+    diff = abs(a - b)
+    print((diff + 9) // 10)
