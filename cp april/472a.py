ac4805b149fcee6ddb0941db3115fb41866e5ca5
kamrul <cdkamrul9@gmail.com>
2025-04-16 20:44:46 +0600

    472a

---
+++ cp april/472a.py
@@ -0,0 +1,16 @@
+import sys
+
+# sys.stdout = open('cp april/output.txt', 'w')
+# sys.stdin = open('cp april/input.txt', 'r')
+
+input = sys.stdin.readline
+
+gi = lambda: list(map(int, input().split()))
+gs = lambda: list(input().split())
+
+n = int(input())
+
+if n & 1:
+    print(9, n - 9)
+else:
+    print(8, n - 8)
